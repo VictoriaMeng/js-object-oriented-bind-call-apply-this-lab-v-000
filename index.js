fa3affa0ec8b4417778856f583b842c1c6b78015
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 21:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ function setThisWithCall(fn, thisValue, arg) {
 
 function setThisWithApply(fn, thisValue, args) {
   return fn.apply(thisValue, args);
+};
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+
 }
